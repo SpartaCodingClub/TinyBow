f8a486515110f0790aed47470b5f84225e40312b
SpartaCodingClub <spartacodingclub.kr@gmail.com>
2025-02-18 00:23:43 +0900

    Feat: 콜리더 통합, 레이어 소팅
    타일 콜리더 통합
    1층으로 내려가면 다리가 Foreground로,
    2층으로 올라가면 Background로 변환

---
+++ Assets/Scripts/Components/Floor.cs
@@ -0,0 +1,34 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Floor : MonoBehaviour
+{
+    [Header("Required")]
+    [SerializeField] private GameObject targetObject;
+
+    [Header("Optional")]
+    [SerializeField] private TilemapRenderer foreground;
+    [SerializeField] private TilemapRenderer background;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(Define.Player) == false)
+        {
+            return;
+        }
+
+        if (foreground != null)
+        {
+            foreground.sortingLayerName = nameof(foreground).FirstCharacterToUpper();
+        }
+
+        if (background != null)
+        {
+            background.sortingLayerName = nameof(background).FirstCharacterToUpper();
+        }
+
+        targetObject.SetActive(true);
+        transform.parent.gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/Utilities/Define.cs
+++ Assets/Scripts/Utilities/Define.cs
@@ -1,4 +1,4 @@
 public class Define
 {
-
+    public static readonly string Player = nameof(Player);
 }
