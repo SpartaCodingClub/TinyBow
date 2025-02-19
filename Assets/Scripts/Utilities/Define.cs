e5ebf528a1a54bc760f0016c21f61e0aab94dfd6
SpartaCodingClub <spartacodingclub.kr@gmail.com>
2025-02-19 18:41:08 +0900

    Fix: 카메라 범위 오류 수정

--- Assets/Scripts/Utilities/Define.cs
+++ Assets/Scripts/Utilities/Define.cs
@@ -14,7 +14,6 @@ public class Define
     public static readonly Color Sky = new(0.8666667f, 0.7764707f, 0.6313726f);
 
     // Texts
-    public static readonly string EditorOnly = nameof(EditorOnly);
     public static readonly string MainRenderer = nameof(MainRenderer);
     public static readonly string MainTex = "_MainTex";
     public static readonly string Player = nameof(Player);
